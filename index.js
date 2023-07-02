580ac283c6a5275e8e6393ab95bb9288bde71d83
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-07-02 15:17:37 -0500

    completes functions lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(2, 5)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(multiplyNum) {
+    return function newFare(fare) {
+        return multiplyNum * fare
+    }
+
+}
+
+function fareDoubler(fareToBeDoubled) {
+    return fareToBeDoubled * 2
+}
+
+function fareTripler(fareToBeTrippled) {
+    return fareToBeTrippled * 3
+}
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function selectDifferentDrivers(array, firstOrLast) {
+    array = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    if (firstOrLast == returnFirstTwoDrivers) {
+        return array.slice(0, 2)
+    } else if (firstOrLast == returnLastTwoDrivers) {
+        return array.slice(2, 5)
+    }
+}
+
+
+
